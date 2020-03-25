886312d2dc9912f2c06568604283123b0d65692b
kko0831 <kko0831@hanmail.net>
2020-03-26 01:26:55 +0900

    6.1.2 graphql-request

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
-var query = `{totalPhotos, totalUsers}`;
+import React from "react";
+import ReactDOM from "react-dom";
+import { request } from "graphql-request";
+
 var url = "http://localhost:4000/graphql";
 
-var opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query })
-};
+var query = `
+  query listUsers {
+    allUsers {
+      avatar
+      name
+    }
+  }
+`;
 
-fetch(url, opts)
-  .then(res => res.json())
-  .then(
-    ({ data }) => `
-    <p>photos: ${data.totalPhotos}</p>
-    <p>users: ${data.totalUsers}</p>
-  `
-  )
-  .then(text => (document.body.innerHTML = text))
-  .catch(console.error);
+var mutation = `
+  mutation populate($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+    }
+  }
+`;
+
+const App = ({ users = [] }) => (
+  <div>
+    {users.map(user => (
+      <div key={user.githubLogin}>
+        <img src={user.avatar} alt="" />
+        {user.name}
+      </div>
+    ))}
+    <button onClick={addUser}>Add User</button>
+  </div>
+);
+
+const render = ({ allUsers = [] }) =>
+  ReactDOM.render(<App users={allUsers} />, document.getElementById("root"));
+
+const addUser = () =>
+  request(url, mutation, { count: 1 })
+    .then(requestAndRender)
+    .catch(console.error);
+
+const requestAndRender = () =>
+  request(url, query)
+    .then(render)
+    .catch(console.error);
+
+requestAndRender();
